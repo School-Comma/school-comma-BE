4be7ff5f5d584dd5e76efeba701bdb283e813ced
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-01-30 21:25:11 +0900

    chore:환경변수설정

--- t.py
+++ t.py
@@ -1,11 +1,12 @@
-from sqlalchemy.schema import CreateTable
-from src.database.models import User, Post
-from src.database.connection import engine
+# from sqlalchemy.schema import CreateTable
+# from src.database.models import User, Post
+# from src.database.connection import engine
 
-print(CreateTable(Post.__table__).compile(engine))
+# print(CreateTable(Post.__table__).compile(engine))
 
 # import secrets
 
-# # 시크릿 키 생성
+# #시크릿 키 생성
 # secret_key = secrets.token_hex(32)  # 32바이트 길이의 시크릿 키 생성
 # print(secret_key)
+
